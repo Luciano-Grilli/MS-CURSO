d4b9333f647639c2731621eb10f3064108ea326d
luciano <lucianogrilli03@gmail.com>
2021-02-24 15:31:24 -0300

    refactorizacion de nombres

--- src/main/java/com/formaciondbi/microservicios/cursos3/MicroserviciosCursos3Application.java
+++ src/main/java/com/formaciondbi/microservicios/cursos3/MicroserviciosCursos3Application.java
@@ -1,5 +1,6 @@
 package com.formaciondbi.microservicios.cursos3;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
